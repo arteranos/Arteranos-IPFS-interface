71a0fdc402aac73a22877d3a8001a374fdb56ef7
willneedit <willneedit@nurfuerspam.de>
2024-11-22 11:18:30 +0100

    Added libcurl.dll as dependency for Unity.Net.Http

---
+++ PluginsGen~/NuGet-Depends-x86/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGet_Depends_x86
+{
+    public class Class1
+    {
+    }
+}
